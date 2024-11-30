8c492d5eb37d009f6cc337a65245b9bdced57641
kalyan12344 <35682339+kalyan12344@users.noreply.github.com>
2024-11-30 14:38:48 -0600

    Update userOrders.js

--- src/frontend/components/userOrders.js
+++ src/frontend/components/userOrders.js
@@ -13,7 +13,7 @@ const UserOrders = () => {
     const fetchOrders = async () => {
       try {
         const response = await axios.get(
-          `"https://emedical-backend.onrender.com/api/orders/${userData._id}`
+          `https://emedical-backend.onrender.com/api/orders/${userData._id}`
         );
         setOrders(response.data);
       } catch (error) {
