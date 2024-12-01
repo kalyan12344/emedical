f7db29e1922bbda3c439166d4a968943c7521b3f
kalyan12344 <35682339+kalyan12344@users.noreply.github.com>
2024-11-30 23:37:35 -0600

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN.split(","), // Allow multiple origins
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"], // Allowed HTTP methods
     credentials: true, // Allow cookies if needed
   })
 );
