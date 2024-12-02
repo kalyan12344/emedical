087d6431ab9c59ff986fe61ef0b641057c137344
kalyan12344 <35682339+kalyan12344@users.noreply.github.com>
2024-12-01 20:26:26 -0600

    Update orders.js

--- src/backend/routes/orders.js
+++ src/backend/routes/orders.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
+
 const Order = require("../models/orders");
 const router = express.Router();
-
+const transporter = nodemailer.createTransport({
+  service: "gmail", // Use your email service provider
+  auth: {
+    user: "kalyanraju90@gmail.com", // Your email
+    pass: "viea bnia kcgc zqje", // Your email password or app password
+  },
+});
 // Create a new order
 router.post("/api/orders", async (req, res) => {
-  const { userId, items, totalPrice, paymentStatus, deliveryAddress } =
-    req.body;
+  const {
+    userId,
+    items,
+    totalPrice,
+    paymentStatus,
+    deliveryAddress,
+    userData,
+  } = req.body;
 
   try {
     const newOrder = new Order({
@@ -14,6 +28,29 @@ router.post("/api/orders", async (req, res) => {
       totalPrice,
       paymentStatus,
       deliveryAddress,
+    });
+
+    const emailContent = `
+      <h1>Payment Confirmation</h1>
+      <p>Dear ${userData?.name},</p>
+      <p>Your payment of <strong>Rs${totalPrice}</strong> was successful.</p>
+      <p>Order Details:</p>
+      <ul>
+        ${items
+          .map((item) => `<li>${item.name} - Quantity: ${item.quantity}</li>`)
+          .join("")}
+      </ul>
+      <p>Your order will be delivered to:</p>
+      <p>${deliveryAddress?.street}, ${deliveryAddress?.city}, ${
+      deliveryAddress?.state
+    }, ${deliveryAddress?.zipCode}</p>
+    `;
+
+    await transporter.sendMail({
+      from: "kalyanraju90@gmail.com",
+      to: userData?.email,
+      subject: "Payment Confirmation",
+      html: emailContent,
     });
 
     const savedOrder = await newOrder.save();
